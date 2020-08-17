cd248cb78df98e2f546327a0d0543b3aa703e55a
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-08-17 23:54:17 +0800

    mongodb fix 1.0

--- src/config/mongoose/default.config.ts
+++ src/config/mongoose/default.config.ts
@@ -1,6 +1,6 @@
 export default {
   id: "default",
-  url: process.env.DEFAULT_URL || "mongodb://localhost:27017/default",
+  url: process.env.DEFAULT_URL || "mongodb://172.17.0.2:27017/default",
   connectionOptions: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
