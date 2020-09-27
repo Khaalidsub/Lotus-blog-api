d435152399932672a3ebd4b68b82c20c920315e6
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2020-09-27 23:32:35 +0800

    authenticate mongodb 3

--- dist/config/mongoose/default.config.js
+++ dist/config/mongoose/default.config.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     id: "blogs",
-    url: process.env.DEFAULT_URL || "mongodb://localhost:27017/blogs?authSource=blog",
+    url: process.env.DEFAULT_URL || "mongodb://localhost:27017/blogs?authSource=blogs",
     connectionOptions: {
-        user: "blog",
+        user: "lotus",
         pass: "tj.Vnp*J52w69r_g",
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- src/config/mongoose/default.config.ts
+++ src/config/mongoose/default.config.ts
@@ -1,8 +1,8 @@
 export default {
   id: "blogs",
-  url: process.env.DEFAULT_URL || "mongodb://localhost:27017/blogs?authSource=blog",
+  url: process.env.DEFAULT_URL || "mongodb://localhost:27017/blogs?authSource=blogs",
   connectionOptions: {
-    user: "blog",
+    user: "lotus",
     pass: "tj.Vnp*J52w69r_g",
     useNewUrlParser: true,
     useUnifiedTopology: true,
