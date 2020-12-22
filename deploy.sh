cdae8b62b32d3398f0ddbb4bb8fc04936688864c
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-23 00:40:01 +0800

    travis automate deploy 1

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 set -e
 ssh-keyscan -H $IP >>~/.ssh/known_hosts
 
-ssh $USER@$IP echo "docker pull sleepinglotus/subaan-blogs-api;docker pull sleepinglotus/chat-client;"
+ls
+
+ssh $USER@$IP echo "docker pull sleepinglotus/subaan-blogs-api;"
