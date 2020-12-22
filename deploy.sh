a2f3b09ed09a38f5e2cdeb485ed65c5fbd9f6f2a
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-23 00:12:33 +0800

    trial on deploying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+ssh-keyscan -H $IP >>~/.ssh/known_hosts
 
-ssh $USER@$IP echo "docker pull sleepinglotus/subaan-blogs-api;"
+ssh $USER@$IP echo "docker pull sleepinglotus/subaan-blogs-api;docker pull sleepinglotus/chat-client;"
