207a6523f14877c852ac17049dbbc019e811324b
khaalid subaan <khaalidsubaan@gmail.com>
2020-12-23 11:14:04 +0800

    deploy docker rebuild compose after each update

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ ssh-keyscan -H $IP >>~/.ssh/known_hosts
 
 scp ./docker-compose.prod.yml $USER@$IP:.
 
-ssh $USER@$IP echo "docker-compose -f docker-compose.prod.yml up -d; docker network inspect blogs-api_default"
+ssh $USER@$IP echo "docker-compose -f docker-compose.prod.yml up -d --build; docker network inspect blogs-api_default"
