542d2666030e5945ebbdb05a575db90e169d2fe9
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-09 23:55:05 +0800

    2nd fix on travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,3 @@
 #!/bin/bash -xe
-# 1 ==> file to check for the ssh key
-# 2 ==> user type
-# 3 ==> ip address of the server
-# 4 ==> project  to copy and paste to the server
-# 5 ==> destination project  to copy and paste to the server
-# 6 ==> file that runs several scripts
-# 7 ==> destination file in the server
-# 8 ==> domain name
 ssh-keyscan -H $IP >>~/.ssh/known_hosts
-scp ./scripts/dockerPull.sh $USER@$IP:./dockerPull.sh
-scp ./nginx/production.conf $USER@$IP:./production.conf
-scp $1    $USER@$IP:./docker-compose.yml && ssh $USER@$IP  "./dockerPull.sh"
+ssh $USER@$IP  "./dockerPull.sh"
